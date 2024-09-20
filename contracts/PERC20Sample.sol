47b0997cdb705d79aba33e789bbfac4f2c0819c0
nacogrijalvacookcdr2rddb <nacogrijalvacookcdr2rddb@hotmail.com>
2024-09-20 14:24:29 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("d070afa0fa6c86e08b0f5c7bafc7ba85ff9ba1b02197de818adeb3e9892cc02e", "d070afa0fa6c86e08b0f5c7bafc7ba85ff9ba1b02197de818adeb3e9892cc02e") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
